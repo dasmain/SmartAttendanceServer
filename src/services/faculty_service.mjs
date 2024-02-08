b7afe955430fe5703f70a55cb6c3c99a73876fbd
Diyan Ali Shaikh <shaikhdiyanali01@gmail.com>
2024-02-08 15:01:21 +0500

    fix

--- src/services/faculty_service.mjs
+++ src/services/faculty_service.mjs
@@ -37,7 +37,7 @@ export default class FacultyService {
         FirstName: firstName,
         LastName: lastName,
         Username: username,
-        Email:email,
+        email:email,
         password: hashedPassword,
         ContactNo: contactno,
         role: "faculty",
