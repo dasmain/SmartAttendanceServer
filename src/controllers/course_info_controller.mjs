74cf63e210ee9215424e44cca73a57cefd21d9f5
Diyan Ali Shaikh <shaikhdiyanali01@gmail.com>
2024-03-10 01:24:58 +0500

    parent details

--- src/controllers/course_info_controller.mjs
+++ src/controllers/course_info_controller.mjs
@@ -2,6 +2,7 @@ import AttendanceService from "../services/attendance_service.mjs";
 import CourseInfoService from "../services/course_info_service.mjs";
 import CourseService from "../services/course_service.mjs";
 import FacultyService from "../services/faculty_service.mjs";
+import ParentService from "../services/parent_service.mjs";
 import PatternUtil from "../utility/pattern_util.mjs";
 import TokenUtil from "../utility/token_util.mjs";
 
@@ -164,6 +165,88 @@ export default class CourseInfoController {
 
       const serviceResponse = await CourseInfoService.getCourseInfoByStudent(
         student_id
+      );
+
+      for (let i = 0; i < serviceResponse.length; i++) {
+        const attendanceResponse =
+          await AttendanceService.getAttendanceByCourse(
+            serviceResponse[i].courseId
+          );
+
+        attendanceResponse.forEach((attendance) => {
+          const { date, topics } = attendance;
+          const courseIndex = serviceResponse.findIndex(
+            (course) => course.courseId === attendance.courseId
+          );
+
+          if (courseIndex !== -1) {
+            if (!serviceResponse[courseIndex].attendance) {
+              serviceResponse[courseIndex].attendance = [];
+            }
+            serviceResponse[courseIndex].attendance.push({ date, topics });
+          }
+        });
+      }
+
+      for (let i = 0; i < serviceResponse.length; i++) {
+        const course = serviceResponse[i];
+        if (course.courseId != null) {
+          const forCourseResponse = await CourseService.getCourseByID(
+            course.courseId
+          );
+
+          if (typeof forCourseResponse === "string") {
+            serviceResponse.splice(i, 1);
+            i--;
+            continue;
+          }
+
+          course.courseId = forCourseResponse;
+
+          if (course.courseId && course.courseId.courseTeacher != null) {
+            const forFacultyResponse =
+              await FacultyService.getFacultyAccountDetails(
+                course.courseId.courseTeacher
+              );
+            course.courseId.courseTeacher = forFacultyResponse.name;
+          }
+        }
+      }
+
+      if (typeof serviceResponse === "string") {
+        res.status(200).json({
+          success: false,
+          data: {},
+          message: serviceResponse,
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          data: serviceResponse,
+          message: "Course Info details fetched successfully",
+        });
+      }
+    } catch (e) {
+      res.status(500).json({
+        success: false,
+        data: {},
+        message: e.message,
+      });
+    }
+  }
+
+  static async apiGetCourseInfoDetailsByStudentForParent(req, res, next) {
+    try {
+      const token = req.headers["authorization"];
+      const tokenDetails = await TokenUtil.getParentDataFromToken(token);
+      const parent_id = tokenDetails.user_id.toString();
+
+      const parentResponse = await ParentService.getParentAccountDetails(
+        parent_id
+      );
+
+      const serviceResponse = await CourseInfoService.getCourseInfoByStudent(
+        parentResponse.studentID
       );
 
       for (let i = 0; i < serviceResponse.length; i++) {
--- src/routes/course_info_routes.mjs
+++ src/routes/course_info_routes.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import checkRequiredFieldsMiddleware from "../middleware/check_required_fields_middleware.mjs";
 import checkFacultyTokenMiddleware from "../middleware/check_faculty_token_middleware.mjs";
 import checkStudentTokenMiddleware from "../middleware/check_student_token_middleware.mjs";
+import checkParentTokenMiddleware from "../middleware/check_parent_token_middleware.mjs";
 import CourseInfoController from "../controllers/course_info_controller.mjs";
 
 const router = express.Router();
@@ -36,6 +37,13 @@ router
   .get(
     checkStudentTokenMiddleware,
     CourseInfoController.apiGetCourseInfoDetailsByStudent
+  );
+
+  router
+  .route(userRoute + "/parent-details")
+  .get(
+    checkParentTokenMiddleware,
+    CourseInfoController.apiGetCourseInfoDetailsByStudentForParent
   );
 
 export default router;
